6752c61a572f22baaac5a8a3427c5fb6e79615b3
Paavan <paavan0402@gmail.com>
2022-05-12 00:43:37 +0530

    Added Java Code to Animate the elements and Apply Hover effects

---
+++ app.js
@@ -0,0 +1,27 @@
+var links = document.querySelectorAll(".link-item");
+var bgimg = document.querySelector(".bgimg");
+
+links.forEach(link => {
+    link.addEventListener("mouseover", (e) => {
+        links.forEach(lnk => {
+            if(lnk === link) {
+                lnk.style.opacity = "1";
+                lnk.style.backgroundColor = "white";
+                lnk.style.color = "black";
+                lnk.style.transform = "scale(1.05)";
+                bgimg.style.filter = "blur(5px)"
+                bgimg.style.transform = "scale(1.02)";
+            }
+        });
+    });
+    link.addEventListener("mouseout", (e) => {
+        links.forEach(lnk => {
+            lnk.style.opacity = ".8";
+            lnk.style.backgroundColor = "#c26987";
+            lnk.style.color = "white";
+            lnk.style.transform = "scale(1)";
+            bgimg.style.filter = "blur(2px)"
+            bgimg.style.transform = "scale(1)";
+        });
+    });
+});
