8a40ed4d9b828b40b83117b7ae168f067596772c
Paavan <paavan0402@gmail.com>
2022-05-12 11:25:10 +0530

    Added the onload animations through JavaScript ( Fixed the bug ). Changed the font sized of elements to give a better look. Changes the font of the .name element. The project is updated and completed as of now.

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 var links = document.querySelectorAll(".link-item");
 var bgimg = document.querySelector(".bgimg");
+var timeout = 0;
+
+window.onload = (e) => {
+    links.forEach(link=>{
+        setTimeout(() => {
+            link.style.opacity = ".8";
+            link.style.transform = "translate(0,0) scale(1)";
+        }, timeout);
+        timeout+=300;
+    })
+}
 
 links.forEach(link => {
     link.addEventListener("mouseover", (e) => {
         links.forEach(lnk => {
             if(lnk === link) {
-                // lnk.style.opacity = "1";
-                // lnk.style.backgroundColor = "white";
-                // lnk.style.color = "black";
-                // lnk.style.transform = "scale(1.05)";
+                lnk.style.opacity = "1";
+                lnk.style.backgroundColor = "white";
+                lnk.style.color = "black";
+                lnk.style.transform = "scale(1.05)";
                 bgimg.style.filter = "blur(5px)"
                 bgimg.style.transform = " translate(0,0) scale(1.02)";
             }
@@ -16,10 +27,10 @@ links.forEach(link => {
     });
     link.addEventListener("mouseout", (e) => {
         links.forEach(lnk => {
-            // lnk.style.opacity = ".8";
-            // lnk.style.backgroundColor = "#86b29d";
-            // lnk.style.color = "white";
-            // lnk.style.transform = "scale(1)";
+            lnk.style.opacity = ".8";
+            lnk.style.backgroundColor = "#86b29d";
+            lnk.style.color = "white";
+            lnk.style.transform = "scale(1)";
             bgimg.style.filter = "blur(2px)"
             bgimg.style.transform = "scale(1)";
         });
